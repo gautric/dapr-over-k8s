5ea284e024d4a856bcc453997f8f29e315aebf73
admin <gautric@redhat.com>
2022-08-20 12:13:19 +0200

    test pub-sub config

---
+++ samples/pub-sub-config/sub-app/app.js
@@ -0,0 +1,41 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+const daprPort = process.env.DAPR_HTTP_PORT || "3500";
+
+// The Dapr endpoint for the state store component to store the tweets.
+const pubsubEndpoint = `http://localhost:${daprPort}/v1.0/publish/pubsub/inventory`;
+
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+// Dapr publishes messages with the application/cloudevents+json content-type
+app.use(express.json({ type: ['application/json', 'application/*+json'] }));
+
+const port = 3000;
+
+// Default product handler.
+app.post('/products', (req, res) => {
+  console.log("🤔 PRODUCT (default): ", req.body);
+  console.log();
+  res.sendStatus(200);
+});
+
+// Specific handler for widgets.
+app.post('/widgets', (req, res) => {
+  console.log("🪛  WIDGET: ", req.body);
+  console.log();
+  res.sendStatus(200);
+});
+
+// Specific handler for gadgets.
+app.post('/gadgets', (req, res) => {
+  console.log("📱 GADGET: ", req.body);
+  console.log();
+  res.sendStatus(200);
+});
+
+app.listen(port, () => console.log(`Node App listening on port ${port}!`));
